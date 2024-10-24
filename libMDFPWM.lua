1cd9526e381a2e73110cb654e26ff907e581283d
Drew <43963132+drucifer-sc@users.noreply.github.com>
2024-10-24 01:59:21 -0700

    Update libMDFPWM.lua
    Fix some embarrassing oversights in handle pointer positioning, packed data stream parsing

--- libMDFPWM.lua
+++ libMDFPWM.lua
@@ -9,18 +9,18 @@ local string,math=_G.string,_G.math
 -- Main functions
 --Usage .parse(fs.open("disk/song.mdfpwm","rb"))
 --Returns table with single function .getSample(sec) and meta table .meta={title,artist,album}
-local function parse(handle) --Parse the file format
-  if not handle or type(handle) ~= "table" or not handle.read then return false,"Incorrect handle provided" end --Basic check if we got a handle
-  if handle.read(7) == "MDFPWM\003" then --Check if the file header is correct
-    local currpos,datalen,artist,title,album,headerlen=handle.seek(),string.unpack("<Is1s1s1",handle.read(772)) --Read length of DFPWM data stream following header
-    local sampleCount=math.ceil(datalen/12000) --Get the sample count
-    handle.seek("set",currpos+headerlen+1) --Reposition the handle as we likely read too much before
-    local dataStream=handle.read(datalen)
+local function parse(handle)
+  if not handle or type(handle) ~= "table" or not handle.read then return false,"Incorrect handle provided" end
+  if handle.read(7) == "MDFPWM\003" then
+    local datalen,artist,title,album,headerlen=string.unpack("<Is1s1s1",handle.read(772))
+    local sampleCount=math.ceil(datalen/12000)
+    handle.seek("set",7+headerlen-1)
+    local dataStream=handle.read(sampleCount*12000)
 	-- Usage .getSample(sec)
 	-- Returns: One second of buffer from both channels indicated by seconds (starting at 1) {left=<6000 bytes DFPWM>,right=<6000 bytes DFPWM>}
-    local function getSample(iNum) --Pull sample from datastream
+    local function getSample(iNum)
       if iNum < 1 or iNum > sampleCount then return nil end
-      local sampleData=dataStream:sub(((iNum-1)*12000)+1,iNum*12000)
+      local sampleData=dataStream:sub(1+((iNum-1)*12000),iNum*12000)
       return {left=sampleData:sub(1,6000),right=sampleData:sub(6001,12000)}
     end
     return {getSample=getSample,meta={len=sampleCount,artist=artist,title=title,album=album}}
@@ -37,7 +37,7 @@ local function meta(handle)
     isMDFPWM=true
     peekData.len,peekData.artist,peekData.title,peekData.album=string.unpack("<Is1s1s1",handle.read(772))
     peekData.len=math.ceil(peekData.len/12000)
-  end handle.seek("set",currpos) --We're only taking a peek, roll pointer back
+  end handle.seek("set",currpos)
   return isMDFPWM and peekData or {false,"Not MDFPWMv3 Formatted"}
 end
 
